094f8c6768292fda93c8907f9868595530f5d4eb
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-10-17 21:25:50 +0900

    달력추가

--- app/src/main/java/com/example/mvvm_schedule/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_schedule/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.mvvm_schedule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvm_schedule.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
     }
 }
